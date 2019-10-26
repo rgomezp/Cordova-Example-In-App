d45c14e52ab6f2d9e63729c9df656b76fdadec30
mikechoch <mdicioccio25@gmail.com>
2019-10-25 18:04:14 -0700

    Changes to example app for outcome events on Android

--- www/js/index.js
+++ www/js/index.js
@@ -21,7 +21,7 @@
 var app = {
 
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -29,12 +29,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         console.log('Received Event: ' + id);
     },
 
@@ -45,24 +45,26 @@ var app = {
 // For Intel XDK and please add this to your app.js.
 
 document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+    // Enable to debug issues.
+    window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
 
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+    var notificationOpenedCallback = function (jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
 
-  window.plugins.OneSignal.setLocationShared(false);
+    window.plugins.OneSignal.setLocationShared(false);
 
-  window.plugins.OneSignal
-    .startInit("77e32082-ea27-42e3-a898-c72e141824ef")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .handleInAppMessageClicked((result)=>{console.log("clicked!!", JSON.stringify(result))})
-    .endInit();
+    window.plugins.OneSignal
+        .startInit("77e32082-ea27-42e3-a898-c72e141824ef")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .handleInAppMessageClicked((result) => {
+            console.log("clicked!!", JSON.stringify(result))
+        })
+        .endInit();
 
-   window.plugins.OneSignal.pauseInAppMessages(app.paused);
+    window.plugins.OneSignal.pauseInAppMessages(app.paused);
 
-  window.plugins.OneSignal.promptLocation();
+    window.plugins.OneSignal.promptLocation();
 
     document.getElementById('sendTags').addEventListener('click', sendTags);
     document.getElementById('getTags').addEventListener('click', getTags);
@@ -72,58 +74,95 @@ document.addEventListener('deviceready', function () {
     document.getElementById('remove_trigger_button').addEventListener('click', removeTrigger);
     document.getElementById('get_trigger_button').addEventListener('click', getTrigger);
 
-    document.getElementById('pauseIAM').addEventListener('click', ()=>{
-      const paused = !app.paused;
-      console.log("will pause:", paused);
-      window.plugins.OneSignal.pauseInAppMessages(paused);
-      app.paused = paused;
+    document.getElementById('send_outcome_button').addEventListener('click', sendOutcome);
+    document.getElementById('send_unique_outcome_button').addEventListener('click', sendUniqueOutcome);
+    document.getElementById('send_outcome_with_value_button').addEventListener('click', sendOutcomeWithValue);
+
+    document.getElementById('pauseIAM').addEventListener('click', () => {
+        const paused = !app.paused;
+        console.log("will pause:", paused);
+        window.plugins.OneSignal.pauseInAppMessages(paused);
+        app.paused = paused;
     });
 }, false);
 
-function sendTags(){
-  window.plugins.OneSignal.sendTags({key1:"test", isAggie:"true"})
+function sendTags() {
+    window.plugins.OneSignal.sendTags({key1: "test", isAggie: "true"})
 }
 
-function getTags(){
-  window.plugins.OneSignal.getTags(function(json){
-    console.log(JSON.stringify(json));
-    document.getElementById('tags').innerHTML = "Tags: "+JSON.stringify(json);
-  });
+function getTags() {
+    window.plugins.OneSignal.getTags(function (json) {
+        console.log(JSON.stringify(json));
+        document.getElementById('tags').innerHTML = "Tags: " + JSON.stringify(json);
+    });
 }
 
-function printSubscription(){
-  window.plugins.OneSignal.getPermissionSubscriptionState((status)=>{
-    document.getElementById('subscription').innerHTML = "Subscription State: "+status.subscriptionStatus.subscribed;
-    document.getElementById('userId').innerHTML = "userId: "+status.subscriptionStatus.userId;
-  });
+function printSubscription() {
+    window.plugins.OneSignal.getPermissionSubscriptionState((status) => {
+        document.getElementById('subscription').innerHTML = "Subscription State: " + status.subscriptionStatus.subscribed;
+        document.getElementById('userId').innerHTML = "userId: " + status.subscriptionStatus.userId;
+    });
 }
 
-function addTrigger(){
+function addTrigger() {
     var triggerKey = document.getElementById('add_trigger_key').value;
     var triggerValue = document.getElementById('add_trigger_value').value;
     console.log("Adding trigger key:", triggerKey, "with trigger value:", triggerValue);
     window.plugins.OneSignal.addTrigger(triggerKey, triggerValue);
-    
+
     checkTrigger(triggerKey);
 }
 
-function checkTrigger(triggerKey){
+function checkTrigger(triggerKey) {
     window.plugins.OneSignal.getTriggerValueForKey(triggerKey, function (triggerValue) {
-       console.log(triggerKey, triggerValue);
+        console.log(triggerKey, triggerValue);
     });
 }
 
-function removeTrigger(){
+function removeTrigger() {
     var triggerKey = document.getElementById('remove_trigger_key').value;
     console.log("Removing trigger with key:", triggerKey);
     window.plugins.OneSignal.removeTriggerForKey(triggerKey);
-    
+
     checkTrigger(triggerKey);
 }
 
-function getTrigger(){
+function getTrigger() {
     var triggerKey = document.getElementById('get_trigger_key').value;
     checkTrigger(triggerKey);
+}
+
+function sendOutcome() {
+    var outcomeName = document.getElementById('send_outcome_name').value;
+    window.plugins.OneSignal.sendOutcome(outcomeName, function (response) {
+        console.log("Successfully sent normal outcome event!");
+        printOutcomeEvent(response);
+    });
+}
+
+function sendUniqueOutcome() {
+    var uniqueOutcomeName = document.getElementById('send_unique_outcome_name').value;
+    window.plugins.OneSignal.sendUniqueOutcome(uniqueOutcomeName, function (response) {
+        console.log("Successfully sent unique outcome event!");
+        printOutcomeEvent(response);
+    });
+}
+
+function sendOutcomeWithValue() {
+    var outcomeName = document.getElementById('send_outcome_with_value_name').value;
+    var outcomeValue = document.getElementById('send_outcome_with_value_value').value;
+    window.plugins.OneSignal.sendOutcomeWithValue(outcomeName, outcomeValue, function (response) {
+        console.log("Successfully sent outcome event with value!");
+        printOutcomeEvent(response);
+    });
+}
+
+function printOutcomeEvent(response) {
+    console.log(response.session + "\n" +
+    response.notification_ids + "\n" +
+    response.id + "\n" +
+    response.timestamp + "\n" +
+    response.weight);
 }
 
 app.initialize();
